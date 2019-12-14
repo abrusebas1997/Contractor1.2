9b6318fef470e7ca44d55b68c30ce37f9959224f
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-12-13 18:02:04 -0800

    fixing settings

--- buycode/settings.py
+++ buycode/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles"),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
