89050722a7674a66399ebff5c0c4383505771305
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-12-13 17:59:43 -0800

    fixinf settings

--- buycode/settings.py
+++ buycode/settings.py
@@ -130,11 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
 
 # codes app settings
 PROJECT_CODE_TITLE_MAX_LENGTH = 600
